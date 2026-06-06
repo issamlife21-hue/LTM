b870a2e4b39d643f20497c695ab7a40c88642fd0
Issam Abou Darwich <issamlife21@gmail.com>
2026-06-05 20:14:43 -0700

    chore: add motion, axe-core, bundle-analyzer, sharp-cli; compress images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+const withAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+}
 
-export default nextConfig;
+export default withAnalyzer(nextConfig)
